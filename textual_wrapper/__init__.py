763c77f103d1cc09067549514abdc7971f05eff9
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-01-01 09:42:10 +0000

    Bump version v0.1.0 -> v0.1.1

--- textual_wrapper/__init__.py
+++ textual_wrapper/__init__.py
@@ -29,5 +29,5 @@ GUI window wrapper around a Textual app (or any other terminal program).
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2026 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 __email__: str = "dominic@davis-foster.co.uk"
